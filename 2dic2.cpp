71bc313107d2f53bc8d245a4e40fb280d8c4a4ce
abhinav3593 <abhinavnadda2003@gmail.com>
2025-07-08 15:35:06 +0530

    This is my second commit

---
+++ 2dic2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <list>
+#include <set>
+using namespace std;
+void solve(){
+    long long n;//size
+    long long m;//length
+    cin>>n>>m;
+    
+
+
+}
+
+int main(){
+    long long t;
+    cin>>t;
+    while (t--)
+    {
+        solve();
+    }
+    
+    return 0;
+}
