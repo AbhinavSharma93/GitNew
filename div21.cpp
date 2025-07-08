f63ada5e66304d179495ac4b2c9937db5ed09fa9
abhinav3593 <abhinavnadda2003@gmail.com>
2025-07-08 15:24:47 +0530

    This is the first commit

---
+++ div21.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <list>
+#include <set>
+using namespace std;
+int numberPrime(int l, int r){
+    int count =0;
+    for (int i = l; i <=r; i++)
+    {
+        for (int j = 2; j <=i; j++)
+        {
+            if(i%j!=0){
+
+            }
+        }
+        
+    }
+    
+
+}
+void solve(){
+
+}
+
+int main(){
+    long long t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
